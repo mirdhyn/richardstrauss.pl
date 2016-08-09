8f0a33829bd6980a95ebe99f530e88a3a1dda7f7
Merlin Gaillard <merlin.gaillard@showmax.com>
2016-08-09 16:50:17 +0200

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+wget https://github.com/spf13/hugo/releases/download/v0.15/hugo_0.15_linux_amd64.tar.gz
+tar xzvf hugo_0.15_linux_amd64.tar.gz
+
+./hugo_0.15_linux_amd64/hugo_0.15_linux_amd64
+
+#echo "www.richardstraus.pl" > public/CNAME
+cd public
+git init
+
+git config user.name "Travis CI"
+git config user.email "mirdhyn@gmail.com"
+
+git add .
+git commit -m "Deploy to GitHub Pages"
+
+git push --force --quiet "https://${GH_TOKEN}@github.com/mirdhyn/richardstaruss.pl.git" master:gh-pages > /dev/null 2>&1
