957f7e1057af30043f77eb67a1f4302db834791e
Merlin Gaillard <merlin.gaillard@showmax.com>
2016-09-15 11:32:51 +0200

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,5 @@
 set -e
 
 wget https://github.com/spf13/hugo/releases/download/v0.16/hugo_0.16_linux-64bit.tgz
-tar xzvf hugo_0.16_linux_amd64.tgz
-
-./hugo_0.16_linux_amd64/hugo_0.16_linux_amd64
+tar xzvf hugo_0.16_linux-64bit.tgz
+./hugo
